56305419e85f098e4fa9bff6830626ef0a598a3b
Agung Firmansyah <agungfirid@gmail.com>
2022-02-18 01:35:20 +0700

    rename app, and other

--- app/src/androidTest/java/com/agungfir/liveattendanceapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/agungfir/liveattendanceapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.agungfir.myoffice", appContext.packageName)
+        assertEquals("com.agungfir.liveattendanceapp", appContext.packageName)
     }
 }
