5258031baef9e77132958a3d44846cbfd05e6df8
Agung Firmansyah <agungfirid@gmail.com>
2022-02-20 23:19:17 +0700

    feat: feature attendance

---
+++ app/src/main/java/com/agungfir/liveattendanceapp/date/MyDate.kt
@@ -0,0 +1,13 @@
+package com.agungfir.liveattendanceapp.date
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object MyDate {
+
+    fun getCurrentDateForServer(): String {
+        val currentTime = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(currentTime)
+    }
+}
--- app/src/main/java/com/agungfir/liveattendanceapp/networking/LiveAttendaceApiService.kt
+++ app/src/main/java/com/agungfir/liveattendanceapp/networking/LiveAttendaceApiService.kt
@@ -1,11 +1,13 @@
 package com.agungfir.liveattendanceapp.networking
 
-import com.agungfir.liveattendanceapp.model.LoginResponse
+import com.agungfir.liveattendanceapp.model.AttendanceResponse
 import com.agungfir.liveattendanceapp.model.ForgotPasswordResponse
+import com.agungfir.liveattendanceapp.model.HistoryResponse
+import com.agungfir.liveattendanceapp.model.LoginResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface LiveAttendaceApiService {
 
@@ -16,4 +18,21 @@ interface LiveAttendaceApiService {
     @Headers("Accept: application/json", "Content-Type: application/json")
     @POST("auth/password/forgot")
     fun forgotPassworRequest(@Body body: String): Call<ForgotPasswordResponse>
+
+    @Multipart
+    @Headers("Accept: application/json")
+    @POST("attendance")
+    fun attend(
+        @Header("Authorization") token: String,
+        @PartMap params: HashMap<String, RequestBody>,
+        @Part photo: MultipartBody.Part
+    ): Call<AttendanceResponse>
+
+    @Headers("Accept: application/json")
+    @GET("attendance/history")
+    fun getHistoryAttendance(
+        @Header("Authorization") token: String,
+        @Query("from") fromDate: String,
+        @Query("to") toDate: String
+    ):Call<HistoryResponse>
 }
